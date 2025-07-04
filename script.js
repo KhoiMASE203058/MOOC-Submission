e2081e4458c7c3f3fc183f8a55e86df8b0b6e7b2
KhoiMASE203058 <maikhoistudent@gmail.com>
2025-07-04 22:31:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function upDate(previewPic) {
-  console.log("Triggered upDate");
-  console.log("Image source:", previewPic.src);
-  console.log("Image alt:", previewPic.alt);
-
+  console.log("Triggered upDate for", previewPic.alt);
   const imageDiv = document.getElementById("image");
-
   imageDiv.innerHTML = previewPic.alt;
   imageDiv.style.backgroundImage = `url('${previewPic.src}')`;
 }
 
 function undo() {
   console.log("Triggered undo");
-
   const imageDiv = document.getElementById("image");
-
   imageDiv.innerHTML = "Hover over an image below to display here.";
   imageDiv.style.backgroundImage = "url('')";
+}
+
+function addTabFocus() {
+  console.log("Page loaded. Adding tabindex to images...");
+  const images = document.querySelectorAll(".preview");
+
+  for (let i = 0; i < images.length; i++) {
+    images[i].setAttribute("tabindex", "0");
+  }
+}
